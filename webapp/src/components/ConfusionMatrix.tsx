ea7915d82e13f09414d719a2473ef28a5d88a4ae
Joseph Marinier <joseph.marinier@servicenow.com>
2022-04-13 13:16:48 -0400

    Unindent
    by removing now unnecessary fragment

--- webapp/src/components/ConfusionMatrix.tsx
+++ webapp/src/components/ConfusionMatrix.tsx
@@ -101,160 +101,158 @@ const ConfusionMatrix: React.FC<Props> = ({
   const maxCount = Math.max(...confusionMatrix.flat()) || 1;
 
   return (
-    <>
+    <Box
+      alignItems="center"
+      display="flex"
+      flexDirection="column"
+      height="100%"
+      justifyContent="center"
+      minHeight={0}
+      width="100%"
+    >
       <Box
         alignItems="center"
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        justifyContent="center"
-        minHeight={0}
-        width="100%"
+        display="grid"
+        gridTemplateColumns={`${theme.spacing(3)} ${theme.spacing(
+          LABEL_WIDTH
+        )}`}
+        gridTemplateRows={`${theme.spacing(3)} ${theme.spacing(LABEL_WIDTH)}`}
+        justifyItems="center"
+        minHeight={200}
+        minWidth={200}
       >
+        <Typography variant="caption" sx={{ gridRow: 1, gridColumn: 3 }}>
+          Prediction
+        </Typography>
+        <Typography
+          className={classes.verticalLabel}
+          sx={{ gridRow: 3, gridColumn: 1 }}
+          variant="caption"
+        >
+          Label
+        </Typography>
         <Box
           alignItems="center"
-          display="grid"
-          gridTemplateColumns={`${theme.spacing(3)} ${theme.spacing(
-            LABEL_WIDTH
-          )}`}
-          gridTemplateRows={`${theme.spacing(3)} ${theme.spacing(LABEL_WIDTH)}`}
+          display="inline-grid"
+          gridAutoColumns={theme.spacing(3.5)}
+          gridAutoFlow="row"
+          gridAutoRows={theme.spacing(3.5)}
+          gridColumn="2 / span 2"
+          gridRow="2 / span 2"
+          gridTemplateRows={theme.spacing(LABEL_WIDTH)}
+          gridTemplateColumns={theme.spacing(LABEL_WIDTH)}
+          height="100%"
           justifyItems="center"
-          minHeight={200}
-          minWidth={200}
+          overflow="scroll"
+          width="100%"
+          sx={{
+            // Stops accidental navigation on horizontal scroll with touch pad
+            overscrollBehaviorX: "contain",
+          }}
         >
-          <Typography variant="caption" sx={{ gridRow: 1, gridColumn: 3 }}>
-            Prediction
-          </Typography>
-          <Typography
-            className={classes.verticalLabel}
-            sx={{ gridRow: 3, gridColumn: 1 }}
-            variant="caption"
-          >
-            Label
-          </Typography>
           <Box
-            alignItems="center"
-            display="inline-grid"
-            gridAutoColumns={theme.spacing(3.5)}
-            gridAutoFlow="row"
-            gridAutoRows={theme.spacing(3.5)}
-            gridColumn="2 / span 2"
-            gridRow="2 / span 2"
-            gridTemplateRows={theme.spacing(LABEL_WIDTH)}
-            gridTemplateColumns={theme.spacing(LABEL_WIDTH)}
-            height="100%"
-            justifyItems="center"
-            overflow="scroll"
-            width="100%"
-            sx={{
-              // Stops accidental navigation on horizontal scroll with touch pad
-              overscrollBehaviorX: "contain",
-            }}
-          >
-            <Box
-              className={classNames(
-                classes.corner,
-                classes.topStickyCell,
-                classes.leftStickyCell
-              )}
-              gridColumn={CONFUSION_COLUMN_OFFSET}
-              gridRow={CONFUSION_ROW_OFFSET}
-            />
-
-            {classOptions.map((prediction, column) => (
-              <Typography
-                key={`column-${column}`}
-                className={classNames(
-                  classes.gridLabel,
-                  classes.columnLabel,
-                  classes.verticalLabel,
-                  classes.topStickyCell,
-                  predictionFilters &&
-                    !predictionFilters.includes(prediction) &&
-                    classes.filteredLabel
-                )}
-                variant="caption"
-                sx={{
-                  gridColumn: column + CONFUSION_COLUMN_OFFSET + 1,
-                  gridRow: 1,
-                }}
-              >
-                {prediction}
-              </Typography>
-            ))}
-            {classOptions.map((label, row) => (
-              <Typography
-                key={`row-${row}`}
-                className={classNames(
-                  classes.gridLabel,
-                  classes.rowLabel,
-                  classes.leftStickyCell,
-                  labelFilters &&
-                    !labelFilters.includes(label) &&
-                    classes.filteredLabel
-                )}
-                variant="caption"
-                sx={{
-                  gridRow: row + CONFUSION_ROW_OFFSET + 1,
-                  gridColumn: 1,
-                }}
-              >
-                {label}
-              </Typography>
-            ))}
-
-            {confusionMatrix.flatMap((row, rowIndex) =>
-              row.map((value, columnIndex) => (
-                <Box
-                  key={`column-${columnIndex} row-${rowIndex}`}
-                  boxShadow={`0 0 0 0.35px ${theme.palette.divider}`}
-                  gridRow={rowIndex + CONFUSION_ROW_OFFSET + 1}
-                  gridColumn={columnIndex + CONFUSION_COLUMN_OFFSET + 1}
-                  height="100%"
-                  overflow="hidden"
-                  width="100%"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  sx={{
-                    backgroundColor: alpha(
-                      theme.palette[
-                        rowIndex === columnIndex
-                          ? columnIndex === row.length - 1
-                            ? OUTCOME_COLOR.CorrectAndRejected
-                            : OUTCOME_COLOR.CorrectAndPredicted
-                          : columnIndex === row.length - 1
-                          ? OUTCOME_COLOR.IncorrectAndRejected
-                          : OUTCOME_COLOR.IncorrectAndPredicted
-                      ].main,
-                      value / maxCount
-                    ),
-                  }}
-                >
-                  {value > 0 && (
-                    <Typography
-                      fontSize={12}
-                      color={(theme) =>
-                        theme.palette.common[value > 0.7 ? "white" : "black"]
-                      }
-                    >
-                      {(value * 100).toFixed(0)}
-                      <Typography
-                        component="span"
-                        fontSize="0.75em"
-                        sx={{ verticalAlign: "0.25em" }}
-                      >
-                        %
-                      </Typography>
-                    </Typography>
-                  )}
-                </Box>
-              ))
+            className={classNames(
+              classes.corner,
+              classes.topStickyCell,
+              classes.leftStickyCell
             )}
-          </Box>
+            gridColumn={CONFUSION_COLUMN_OFFSET}
+            gridRow={CONFUSION_ROW_OFFSET}
+          />
+
+          {classOptions.map((prediction, column) => (
+            <Typography
+              key={`column-${column}`}
+              className={classNames(
+                classes.gridLabel,
+                classes.columnLabel,
+                classes.verticalLabel,
+                classes.topStickyCell,
+                predictionFilters &&
+                  !predictionFilters.includes(prediction) &&
+                  classes.filteredLabel
+              )}
+              variant="caption"
+              sx={{
+                gridColumn: column + CONFUSION_COLUMN_OFFSET + 1,
+                gridRow: 1,
+              }}
+            >
+              {prediction}
+            </Typography>
+          ))}
+          {classOptions.map((label, row) => (
+            <Typography
+              key={`row-${row}`}
+              className={classNames(
+                classes.gridLabel,
+                classes.rowLabel,
+                classes.leftStickyCell,
+                labelFilters &&
+                  !labelFilters.includes(label) &&
+                  classes.filteredLabel
+              )}
+              variant="caption"
+              sx={{
+                gridRow: row + CONFUSION_ROW_OFFSET + 1,
+                gridColumn: 1,
+              }}
+            >
+              {label}
+            </Typography>
+          ))}
+
+          {confusionMatrix.flatMap((row, rowIndex) =>
+            row.map((value, columnIndex) => (
+              <Box
+                key={`column-${columnIndex} row-${rowIndex}`}
+                boxShadow={`0 0 0 0.35px ${theme.palette.divider}`}
+                gridRow={rowIndex + CONFUSION_ROW_OFFSET + 1}
+                gridColumn={columnIndex + CONFUSION_COLUMN_OFFSET + 1}
+                height="100%"
+                overflow="hidden"
+                width="100%"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                sx={{
+                  backgroundColor: alpha(
+                    theme.palette[
+                      rowIndex === columnIndex
+                        ? columnIndex === row.length - 1
+                          ? OUTCOME_COLOR.CorrectAndRejected
+                          : OUTCOME_COLOR.CorrectAndPredicted
+                        : columnIndex === row.length - 1
+                        ? OUTCOME_COLOR.IncorrectAndRejected
+                        : OUTCOME_COLOR.IncorrectAndPredicted
+                    ].main,
+                    value / maxCount
+                  ),
+                }}
+              >
+                {value > 0 && (
+                  <Typography
+                    fontSize={12}
+                    color={(theme) =>
+                      theme.palette.common[value > 0.7 ? "white" : "black"]
+                    }
+                  >
+                    {(value * 100).toFixed(0)}
+                    <Typography
+                      component="span"
+                      fontSize="0.75em"
+                      sx={{ verticalAlign: "0.25em" }}
+                    >
+                      %
+                    </Typography>
+                  </Typography>
+                )}
+              </Box>
+            ))
+          )}
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
