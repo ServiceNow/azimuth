61b4c1a8b26182fc0616a5e83279bf85be3edcbe
nandhini.babu <nandhini.babu@servicenow.com>
2022-06-30 14:40:14 -0400

    Adding test case to Dashboard - part1

--- webapp/src/components/Description.test.tsx
+++ webapp/src/components/Description.test.tsx
@@ -5,7 +5,7 @@ import { renderWithTheme } from "mocks/utils";
 test("display description with just text", async () => {
   renderWithTheme(<Description text="test description" />);
 
-  screen.getByText("test description");
+  expect(screen.getByText("test description")).toBeInTheDocument();
 });
 
 test("display description with both link and text", async () => {
@@ -16,10 +16,21 @@ test("display description with both link and text", async () => {
     />
   );
 
-  screen.getByText("test description");
+  expect(screen.getByText("test description")).toBeInTheDocument();
 
   const link: HTMLAnchorElement = screen.getByRole("link");
   expect(link.href).toContain("https://servicenow.github.io/azimuth/");
-  screen.getByText("Learn more");
-  screen.getByTestId("LinkIcon");
+  expect(screen.getByText("Learn more")).toBeInTheDocument();
+  expect(screen.getByTestId("LinkIcon")).toBeInTheDocument();
+});
+
+test("verify conditional rendering of text display if available", async () => {
+  renderWithTheme(<Description link="https://servicenow.github.io/azimuth/" />);
+  expect(screen.queryByText("test description")).toBeNull();
+});
+
+test("verify conditional rendering of link display if available", async () => {
+  renderWithTheme(<Description text="test description" />);
+  expect(screen.queryByText("Learn more")).toBeNull();
+  expect(screen.queryByTestId("LinkIcon")).toBeNull();
 });
