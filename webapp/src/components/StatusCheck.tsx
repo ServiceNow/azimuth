957989145596168c88f6e7c97af7ea3562c1d4fb
Dref360 <frederic.branchaud.charron@gmail.com>
2022-07-27 18:14:24 -0400

    Add progress info during startup

--- webapp/src/components/StatusCheck.tsx
+++ webapp/src/components/StatusCheck.tsx
@@ -1,13 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { Box, capitalize, Stack, Typography } from "@mui/material";
 import noData from "assets/launch.svg";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getStatusEndpoint } from "services/api";
 import Loading from "./Loading";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import CheckIcon from "./Icons/Check";
+import ErrorIcon from "@mui/icons-material/Error";
+import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
 
 type Props = {
   children: React.ReactNode;
 };
+
+interface StringByString {
+  [key: string]: any;
+}
+
+const STATUS_ICON_MAPPING: StringByString = {
+  finished: <CheckIcon />,
+  not_started: <CheckIcon />,
+  pending: <PendingOutlinedIcon />,
+  error: <ErrorIcon />,
+  lost: <ErrorIcon />,
+};
+
+const render_cell = (task_name: string, task_status: string) => (
+  <Grid item xs={1.5}>
+    <Paper variant="outlined">
+      <Box display="flex" alignItems="center" gap={1}>
+        <Typography>
+          {`${task_name.split("_").map(capitalize).join(" ")} `} {STATUS_ICON_MAPPING[task_status]}
+        </Typography>
+      </Box>
+    </Paper>
+  </Grid>
+);
 
 const StatusCheck: React.FC<Props> = ({ children }) => {
   const { jobId } = useParams<{ jobId: string }>();
@@ -31,20 +60,25 @@ const StatusCheck: React.FC<Props> = ({ children }) => {
         justifyContent="center"
         alignItems="center"
         width="100%"
-        height="75vh"
+        height="100vh"
       >
         <Box
           display="flex"
           flexDirection="column"
           alignItems="center"
           gap={4}
-          width={600}
+          width={700}
         >
           <img src={noData} alt="Startup tasks still in progress" width={400} />
           <Typography variant="h2" align="center">
             The startup tasks are still in progress. Grab a coffee and we will
             auto-refresh for you.
           </Typography>
+          <Grid container spacing={2} columnSpacing={3} columns={3}>
+            {Object.entries(status.startupTasksStatus).map(
+              ([task_name, task_status]) => render_cell(task_name, task_status)
+            )}
+          </Grid>
         </Box>
       </Box>
     );
