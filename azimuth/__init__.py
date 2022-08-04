7f3b4c9c8973b81e27047c7b70176331413c6b6c
gabrielle.gm <gabrielle.gm@servicenow.com>
2022-08-03 21:49:58 -0400

    Downgrade datasets

--- azimuth/__init__.py
+++ azimuth/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from datasets import disable_progress_bar
+from datasets import set_progress_bar_enabled
 from datasets.utils.logging import set_verbosity_error
 
 from azimuth.utils.logs import set_logger_config
@@ -8,5 +8,5 @@ from azimuth.utils.logs import set_logger_config
 PROJECT_ROOT = str(Path(__file__).parent)
 
 set_verbosity_error()
-disable_progress_bar()
+set_progress_bar_enabled(False)
 set_logger_config()
