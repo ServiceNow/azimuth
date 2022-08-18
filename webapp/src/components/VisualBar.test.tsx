b477755a8bcf93d4713261eba549b920127d667d
nandhini.babu <nandhini.babu@servicenow.com>
2022-08-18 13:48:25 -0400

    Performance analysis code coverage - VisualBar

---
+++ webapp/src/components/VisualBar.test.tsx
@@ -0,0 +1,23 @@
+import { BoxProps } from "@mui/material";
+import { fireEvent, screen } from "@testing-library/react";
+import { renderWithTheme } from "mocks/utils";
+import VisualBar from "./VisualBar";
+
+const renderVisualBar = (
+  formattedValue: string,
+  value: number,
+  color: BoxProps["bgcolor"]
+) =>
+  renderWithTheme(
+    <VisualBar formattedValue={formattedValue} value={value} color={color} />
+  );
+
+describe("VisualBar", () => {
+  it("should render expected value with correct styling (height, width, bgcolor", () => {
+    renderVisualBar("39.1%", 0.39134, "#00B686");
+    expect(screen.getByText("39.1%")).toBeVisible();
+    expect(
+      screen.getByText("39.1%").parentElement!.lastChild!.firstChild
+    ).toHaveStyle("width:39.134%;height:90%;background-color:#00B686");
+  });
+});
