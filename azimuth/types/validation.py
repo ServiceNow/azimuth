b855cb5e10fa22b7bfb599c5b1552c155158c61a
Gabrielle Gauthier Melancon <gabrielle.gm@servicenow.com>
2022-11-25 11:20:22 -0500

    Add missing copyrights (#293)

--- azimuth/types/validation.py
+++ azimuth/types/validation.py
@@ -1,3 +1,6 @@
+# Copyright ServiceNow, Inc. 2021 – 2022
+# This source code is licensed under the Apache 2.0 license found in the LICENSE file
+# in the root directory of this source tree.
 from azimuth.types import AliasModel
 
 
