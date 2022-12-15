7a40572a0209204a80c7cb40eecacef6e689c4e2
joseph.marinier <joseph.marinier@servicenow.com>
2022-12-15 16:59:27 -0500

    Fix typo

--- webapp/src/utils/format.ts
+++ webapp/src/utils/format.ts
@@ -13,5 +13,5 @@ export const formatRatioAsPercentageString = (
 ) => `${formatNumberAsString(100 * value, digits)}%`;
 
 export const camelToTitleCase = (camelCase: string) =>
-  // Safari and iOS browsers don't support lookbehind is regular expressions.
+  // Safari and iOS browsers don't support lookbehind in regular expressions.
   camelCase.replace(/([a-z])(?=[A-Z0-9])|([A-Z0-9])(?=[A-Z][a-z])/g, "$1$2 ");
