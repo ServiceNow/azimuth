515fc14dc3e8ff2264361259979821c2c9e00f9c
joseph.marinier <joseph.marinier@servicenow.com>
2022-12-23 18:42:00 -0500

    Fix fields of type `Any` and `Union`

--- azimuth/utils/exclude_fields_with_extra.py
+++ azimuth/utils/exclude_fields_with_extra.py
@@ -5,12 +5,25 @@ from pydantic.fields import SHAPE_SINGLETON, ModelField
 
 
 def exclude_fields_with_extra(fields: Dict[str, ModelField], extra: str):
+    """Exclude pydantic fields with given extra keyword argument.
+
+    Args:
+        fields: Usually pydantic.BaseModel.__field__.
+        extra: The extra keyword argument to look for in each field.
+
+    Returns: The format expected by pydantic.BaseModel.dict(exclude=...), json() and _iter()
+
+    """
     exclude = dict()
     for key, field in fields.items():
         if field.field_info.extra.get(extra):
             exclude[key] = ...  # TODO update pydantic to >=1.9 and change `...` to `True`
-        elif issubclass(field.type_, BaseModel):
-            sub = exclude_fields_with_extra(field.type_.__fields__, extra)
-            if sub:
-                exclude[key] = sub if field.shape == SHAPE_SINGLETON else {"__all__": sub}
+        else:
+            try:
+                if issubclass(field.type_, BaseModel):
+                    sub = exclude_fields_with_extra(field.type_.__fields__, extra)
+                    if sub:
+                        exclude[key] = sub if field.shape == SHAPE_SINGLETON else {"__all__": sub}
+            except TypeError:
+                pass
     return exclude
