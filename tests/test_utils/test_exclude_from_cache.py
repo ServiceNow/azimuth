4845fabfe98ecdb531583ca939253d5e24eeb393
joseph.marinier <joseph.marinier@servicenow.com>
2023-01-03 13:10:51 -0500

    Test field of any type

--- tests/test_utils/test_exclude_from_cache.py
+++ tests/test_utils/test_exclude_from_cache.py
@@ -2,7 +2,7 @@
 # This source code is licensed under the Apache 2.0 license found in the LICENSE file
 # in the root directory of this source tree.
 
-from typing import Dict, List, Union
+from typing import Any, Dict, List, Union
 
 from pydantic import BaseModel, Field
 
@@ -23,7 +23,8 @@ class Model(BaseModel):
     excluded_sub_with_excluded_field: SubWithExcludedField = Field(..., exclude_from_cache=True)
     sub_with_excluded_field: SubWithExcludedField
     list_of_sub_with_excluded_field: List[SubWithExcludedField]
-    list_of_any: List
+    any: Any
+    list_of_any: List[Any]
     list_of_union: List[Union[SubWithExcludedField, Sub, int]]
     dict_of_union: Dict[str, Union[SubWithExcludedField, Sub, int]]
 
@@ -34,6 +35,7 @@ def test_exclude_from_cache():
         excluded_sub_with_excluded_field=SubWithExcludedField(excluded_field=1, field=7),
         sub_with_excluded_field=SubWithExcludedField(excluded_field=2, field=8),
         list_of_sub_with_excluded_field=[SubWithExcludedField(excluded_field=3, field=5)],
+        any=SubWithExcludedField(excluded_field=-1, field=-2),
         list_of_any=[SubWithExcludedField(excluded_field=4, field=6), Sub(field=10), 11, "hi!"],
         list_of_union=[SubWithExcludedField(excluded_field=4, field=6), Sub(field=10), 11],
         dict_of_union={
@@ -47,6 +49,7 @@ def test_exclude_from_cache():
         "excluded_sub_with_excluded_field": ...,
         "sub_with_excluded_field": {"excluded_field": ...},
         "list_of_sub_with_excluded_field": {0: {"excluded_field": ...}},
+        "any": {"excluded_field": ...},
         "list_of_any": {0: {"excluded_field": ...}},
         "list_of_union": {0: {"excluded_field": ...}},
         "dict_of_union": {"a": {"excluded_field": ...}},
@@ -55,6 +58,7 @@ def test_exclude_from_cache():
         "sub": {"field": 9},
         "sub_with_excluded_field": {"field": 8},
         "list_of_sub_with_excluded_field": [{"field": 5}],
+        "any": {"field": -2},
         "list_of_any": [{"field": 6}, {"field": 10}, 11, "hi!"],
         "list_of_union": [{"field": 6}, {"field": 10}, 11],
         "dict_of_union": {"a": {"field": 6}, "b": {"field": 10}, "c": 11},
