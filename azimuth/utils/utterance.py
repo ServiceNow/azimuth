d60066aa296f83ef61e4c8f5075d418cd2ba527f
gabrielle.gm <gabrielle.gm@servicenow.com>
2023-01-25 14:40:47 -0500

    Move reg ex

--- azimuth/utils/utterance.py
+++ azimuth/utils/utterance.py
@@ -3,6 +3,8 @@
 # in the root directory of this source tree.
 import re
 
+re_irregular_apostrophes = re.compile("[’`]")
+
 
 def clean_utterance(utterance: str) -> str:
     """Converts characters as appropriate for downstream functions (e.g., search, spaCy).
@@ -14,6 +16,5 @@ def clean_utterance(utterance: str) -> str:
         utterance_clean: Cleaned utterance.
 
     """
-    re_irregular_apostrophes = re.compile("[’`]")
     utterance_clean = re_irregular_apostrophes.sub("'", utterance).lower()
     return utterance_clean
