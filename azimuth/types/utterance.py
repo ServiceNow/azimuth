3af232d9ddb6d03908c230cf2d2f8191e3b4dcf8
gabrielle.gm <gabrielle.gm@servicenow.com>
2023-02-09 16:43:30 -0500

    Add route to import proposed actions

--- azimuth/types/utterance.py
+++ azimuth/types/utterance.py
@@ -37,15 +37,18 @@ class ModelSaliency(AliasModel):
     saliencies: List[float] = Field(..., title="Saliency")
 
 
-class Utterance(ValuePerDatasetSmartTag[str], ValuePerPipelineSmartTag[str], AliasModel):
-    index: int = Field(..., title="Index", description="Row index computed by Azimuth..")
+class UtterancePatch(AliasModel):
     # Union[int, str] in this order so FastAPI tries to cast to int() first, then defaults to str().
     persistent_id: Union[int, str] = Field(..., title="Persistent id")
+    data_action: DataAction = Field(..., title="Data action tag")
+
+
+class Utterance(ValuePerDatasetSmartTag[str], ValuePerPipelineSmartTag[str], UtterancePatch):
+    index: int = Field(..., title="Index", description="Row index computed by Azimuth..")
     model_prediction: Optional[ModelPrediction] = Field(
         ..., title="Model prediction", nullable=True
     )
     model_saliency: Optional[ModelSaliency] = Field(..., title="Model saliency", nullable=True)
-    data_action: DataAction = Field(..., title="Data action tag")
     label: str = Field(..., title="Label")
     utterance: str = Field(..., title="Utterance")
 
