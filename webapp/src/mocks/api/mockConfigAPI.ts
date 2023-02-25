ServiceNow/azimuth

commit fb6bf4ea42aaa0051fe518fdd24271e8f8e359df

Test for performance analysis comparison table (#420)
* Test script for PipelineSelect,DeltaComputationBar

* Test script for Performance comparison table

* Review comments - code simplified, typo correction

* Review comments - code simplified, typo correction, add test case for negative delta values

* mock API data corrected to latest

* Review comments - corrected passing arguments to deltacomputation, replaced toHaveTextContent to textcontent

webapp/src/mocks/api/mockConfigAPI.ts
@@ -1,0 +1,304 @@
+import { rest } from "msw";
+import { AzimuthConfig } from "types/api";
+
+const baseUrl = "http://localhost/api/local";
+
+export const getConfigAPIResponse = rest.get(
+  `${baseUrl}/admin/config`,
+  (req, res, ctx) => {
+    const azimuthConfigResponse: AzimuthConfig = {
+      name: "Sentiment Analysis",
+      dataset: {
+        class_name: "loading_resources.load_sst2_dataset",
+        args: [],
+        kwargs: {},
+        remote: "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+      },
+      columns: {
+        text_input: "utterance",
+        raw_text_input: "utterance_raw",
+        label: "label",
+        failed_parsing_reason: "failed_parsing_reason",
+        persistent_id: "row_idx",
+      },
+      rejection_class: null,
+      artifact_path: "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/cache",
+      batch_size: 32,
+      use_cuda: "auto",
+      large_dask_cluster: false,
+      read_only_config: false,
+      dataset_warnings: {
+        min_num_per_class: 20,
+        max_delta_class_imbalance: 0.5,
+        max_delta_representation: 0.05,
+        max_delta_mean_words: 3.0,
+        max_delta_std_words: 3.0,
+      },
+      model_contract: "hf_text_classification",
+      pipelines: [
+        {
+          name: "Pipeline_0",
+          model: {
+            class_name: "loading_resources.load_hf_text_classif_pipeline",
+            args: [],
+            kwargs: {
+              checkpoint_path:
+                "distilbert-base-uncased-finetuned-sst-2-english",
+            },
+            remote:
+              "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+          },
+          postprocessors: [
+            {
+              class_name: "azimuth.utils.ml.postprocessing.Thresholding",
+              args: [],
+              kwargs: { threshold: 0.5 },
+              remote: null,
+              threshold: 0.5,
+            },
+          ],
+        },
+      ],
+      uncertainty: { iterations: 1, high_epistemic_threshold: 0.1 },
+      saliency_layer: "distilbert.embeddings.word_embeddings",
+      syntax: {
+        short_utterance_max_word: 3,
+        long_utterance_min_word: 12,
+        spacy_model: "en_core_web_sm",
+        subj_tags: ["nsubj", "nsubjpass"],
+        obj_tags: ["dobj", "pobj", "obj"],
+      },
+      metrics: {
+        Accuracy: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "accuracy" },
+          remote: null,
+          additional_kwargs: {},
+        },
+        Precision: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "precision" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+        Recall: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "recall" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+        F1: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "f1" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+      },
+      language: "en",
+      similarity: {
+        faiss_encoder: "all-MiniLM-L12-v2",
+        conflicting_neighbors_threshold: 0.9,
+        no_close_threshold: 0.5,
+      },
+      behavioral_testing: {
+        neutral_token: {
+          threshold: 1.0,
+          suffix_list: ["pls", "please", "thank you", "appreciated"],
+          prefix_list: ["pls", "please", "hello", "greetings"],
+        },
+        punctuation: { threshold: 1.0 },
+        fuzzy_matching: { threshold: 1.0 },
+        typo: { threshold: 1.0, nb_typos_per_utterance: 1 },
+        seed: 300,
+      },
+    };
+    return res(ctx.json(azimuthConfigResponse));
+  }
+);
+
+export const getConfigMultipipelineAPIResponse = rest.get(
+  `${baseUrl}/admin/config`,
+  (req, res, ctx) => {
+    const azimuthConfigMultipipelineResponse: AzimuthConfig = {
+      name: "Sentiment Analysis",
+      dataset: {
+        class_name: "loading_resources.load_sst2_dataset",
+        args: [],
+        kwargs: {},
+        remote: "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+      },
+      columns: {
+        text_input: "utterance",
+        raw_text_input: "utterance_raw",
+        label: "label",
+        failed_parsing_reason: "failed_parsing_reason",
+        persistent_id: "row_idx",
+      },
+      rejection_class: null,
+      artifact_path: "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/cache",
+      batch_size: 32,
+      use_cuda: "auto",
+      large_dask_cluster: false,
+      read_only_config: false,
+      dataset_warnings: {
+        min_num_per_class: 20,
+        max_delta_class_imbalance: 0.5,
+        max_delta_representation: 0.05,
+        max_delta_mean_words: 3.0,
+        max_delta_std_words: 3.0,
+      },
+      model_contract: "hf_text_classification",
+      pipelines: [
+        {
+          name: "Pipeline_0",
+          model: {
+            class_name: "loading_resources.load_hf_text_classif_pipeline",
+            args: [],
+            kwargs: {
+              checkpoint_path:
+                "distilbert-base-uncased-finetuned-sst-2-english",
+            },
+            remote:
+              "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+          },
+          postprocessors: [
+            {
+              class_name: "azimuth.utils.ml.postprocessing.Thresholding",
+              args: [],
+              kwargs: { threshold: 0.5 },
+              remote: null,
+              threshold: 0.5,
+            },
+          ],
+        },
+        {
+          name: "Pipeline_1",
+          model: {
+            class_name: "loading_resources.load_hf_text_classif_pipeline",
+            args: [],
+            kwargs: {
+              checkpoint_path:
+                "distilbert-base-uncased-finetuned-sst-2-english",
+            },
+            remote:
+              "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+          },
+          postprocessors: [
+            {
+              class_name: "azimuth.utils.ml.postprocessing.Thresholding",
+              args: [],
+              kwargs: { threshold: 0.5 },
+              remote: null,
+              threshold: 0.5,
+            },
+          ],
+        },
+        {
+          name: "Pipeline_2",
+          model: {
+            class_name: "loading_resources.load_hf_text_classif_pipeline",
+            args: [],
+            kwargs: {
+              checkpoint_path:
+                "distilbert-base-uncased-finetuned-sst-2-english",
+            },
+            remote:
+              "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+          },
+          postprocessors: [
+            {
+              class_name: "azimuth.utils.ml.postprocessing.Thresholding",
+              args: [],
+              kwargs: { threshold: 0.5 },
+              remote: null,
+              threshold: 0.5,
+            },
+          ],
+        },
+        {
+          name: "Pipeline_3",
+          model: {
+            class_name: "loading_resources.load_hf_text_classif_pipeline",
+            args: [],
+            kwargs: {
+              checkpoint_path:
+                "distilbert-base-uncased-finetuned-sst-2-english",
+            },
+            remote:
+              "/Users/nandhini.babu/OpenSource-Azimuth/azimuth/azimuth_shr",
+          },
+          postprocessors: [
+            {
+              class_name: "azimuth.utils.ml.postprocessing.Thresholding",
+              args: [],
+              kwargs: { threshold: 0.5 },
+              remote: null,
+              threshold: 0.5,
+            },
+          ],
+        },
+      ],
+      uncertainty: { iterations: 1, high_epistemic_threshold: 0.1 },
+      saliency_layer: "distilbert.embeddings.word_embeddings",
+      syntax: {
+        short_utterance_max_word: 3,
+        long_utterance_min_word: 12,
+        spacy_model: "en_core_web_sm",
+        subj_tags: ["nsubj", "nsubjpass"],
+        obj_tags: ["dobj", "pobj", "obj"],
+      },
+      metrics: {
+        Accuracy: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "accuracy" },
+          remote: null,
+          additional_kwargs: {},
+        },
+        Precision: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "precision" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+        Recall: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "recall" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+        F1: {
+          class_name: "datasets.load_metric",
+          args: [],
+          kwargs: { path: "f1" },
+          remote: null,
+          additional_kwargs: { average: "weighted" },
+        },
+      },
+      language: "en",
+      similarity: {
+        faiss_encoder: "all-MiniLM-L12-v2",
+        conflicting_neighbors_threshold: 0.9,
+        no_close_threshold: 0.5,
+      },
+      behavioral_testing: {
+        neutral_token: {
+          threshold: 1.0,
+          suffix_list: ["pls", "please", "thank you", "appreciated"],
+          prefix_list: ["pls", "please", "hello", "greetings"],
+        },
+        punctuation: { threshold: 1.0 },
+        fuzzy_matching: { threshold: 1.0 },
+        typo: { threshold: 1.0, nb_typos_per_utterance: 1 },
+        seed: 300,
+      },
+    };
+    return res(ctx.json(azimuthConfigMultipipelineResponse));
+  }
+);

webapp/src/components/Analysis/PerturbationTestingPreview.test.tsx
@@ -97,13 +97,13 @@ describe("PerturbationTestingPreview", () => {
     renderPerturbationTestingPreview({ train: true, eval: true });
     const header = screen.getByRole("columnheader", { name: /Failure Rate/ });
     // before toggle changes
-    expect(header).toHaveTextContent("Evaluation Set");
+    expect(header.textContent).toBe("FR on Evaluation Set");
     expect(
       screen.getByLabelText("Failure Rate on Evaluation Set")
     ).toBeInTheDocument();
     // after toggle changes
     fireEvent.click(screen.getByRole("button", { pressed: false }));
-    expect(header).toHaveTextContent("Training Set");
+    expect(header.textContent).toBe("FR on Training Set");
     expect(
       screen.getByLabelText("Failure Rate on Training Set")
     ).toBeInTheDocument();

webapp/src/components/Analysis/PreviewCard.test.tsx
@@ -28,7 +28,7 @@ describe("PreviewCard", () => {
     ).toBeVisible();
     // verify the documentation link
     const link: HTMLAnchorElement = screen.getByRole("link");
-    expect(link).toHaveTextContent("Learn more");
+    expect(link.textContent).toBe("Learn more");
     expect(link.href).toContain("/#performance-analysis");
     expect(screen.getByTestId("OpenInNewIcon")).toBeInTheDocument();
     // verify if the view details is not displayed if 'to' param is unavailable

webapp/src/components/Metrics/DeltaComputationBar.test.tsx
@@ -1,0 +1,44 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "mocks/utils";
+import DeltaComputationBar from "./DeltaComputationBar";
+
+const renderDeltaComputationBar = (
+  value: number,
+  formattedValue: string,
+  width: number
+) =>
+  renderWithTheme(
+    <DeltaComputationBar
+      formattedValue={formattedValue}
+      value={value}
+      width={width}
+    />
+  );
+
+describe("DeltaComputationBar", () => {
+  it("should display the bar and the formatter value with expected bgcolor, width and position if the value is greater than 0", () => {
+    renderDeltaComputationBar(45.5, "45.5", 45.5);
+    expect(screen.getByTestId("delta-computation-bar")).toHaveStyle(
+      "width:45.5%;background-color:rgba(99, 204, 255, 0.8);left:50%"
+    );
+    expect(screen.getByTestId("delta-computation-value").textContent).toBe(
+      "+45.5"
+    );
+    expect(screen.getByTestId("delta-computation-value")).toHaveStyle(
+      "color:rgb(0, 109, 179);right:55%"
+    );
+  });
+
+  it("should display the bar with expected bgcolor, width and position if the value is less than 0", () => {
+    renderDeltaComputationBar(-45.5, "-45.5", 45.5);
+    expect(screen.getByTestId("delta-computation-bar")).toHaveStyle(
+      "width:45.5%;background-color:rgba(37, 36, 54, 0.6);right:50%"
+    );
+    expect(screen.getByTestId("delta-computation-value").textContent).toBe(
+      "-45.5"
+    );
+    expect(screen.getByTestId("delta-computation-value")).toHaveStyle(
+      "color: rgb(37, 36, 54);left:55%"
+    );
+  });
+});

webapp/src/components/Metrics/DeltaComputationBar.tsx
@@ -31,6 +31,7 @@ const DeltaComputationBar: React.FC<Props> = ({
       {value ? (
         <Box width="100%" flex="1" position="relative" marginY={1}>
           <Box
+            data-testid="delta-computation-bar"
             component={motion.div}
             position="absolute"
             overflow="auto"
@@ -49,6 +50,7 @@ const DeltaComputationBar: React.FC<Props> = ({
           ></Box>
 
           <Typography
+            data-testid="delta-computation-value"
             position="absolute"
             sx={{
               ...(value > 0 ? { right: "55%" } : { left: "55%" }),

webapp/src/components/Metrics/PerformanceAnalysisTable.test.tsx
@@ -1,30 +1,38 @@
 import { fireEvent, screen, waitFor, within } from "@testing-library/react";
 import { renderWithRouterAndRedux } from "mocks/utils";
-import PerformanceAnalysis from "./PerformanceAnalysis";
-import { AvailableDatasetSplits } from "types/api";
+import PerformanceAnalysisTable from "./PerformanceAnalysisTable";
+import { AvailableDatasetSplits, DatasetSplitName } from "types/api";
 import { setupServer } from "msw/node";
 import {
   getCustomMetricInfoAPIResponse,
   getMetricsPerFilterAPIResponse,
   getMetricsPerFilterAPIWithFailureResponse,
 } from "mocks/api/mockMetricsAPI";
+import {
+  getConfigAPIResponse,
+  getConfigMultipipelineAPIResponse,
+} from "mocks/api/mockConfigAPI";
 
 const setDatasetSplitName = jest.fn();
-const renderPerformanceAnalysis = (
-  availableDatasetSplits: AvailableDatasetSplits
+const renderPerformanceAnalysisTable = (
+  availableDatasetSplits: AvailableDatasetSplits,
+  datasetSplitName: DatasetSplitName
 ) =>
   renderWithRouterAndRedux(
-    <PerformanceAnalysis
+    <PerformanceAnalysisTable
       jobId="local"
       pipeline={{ pipelineIndex: 0 }}
       availableDatasetSplits={availableDatasetSplits}
-      datasetSplitName="eval"
+      isLoading={false}
+      datasetSplitName={datasetSplitName}
       setDatasetSplitName={setDatasetSplitName}
     />,
     { route: "/local?pipeline_index=0", path: "/:jobId?:pipeline" }
   );
-describe("PerformanceAnalysis", () => {
+
+describe("PerformanceAnalysisComparisonTableWithNoComparedPipeline", () => {
   const handlers = [
+    getConfigAPIResponse,
     getCustomMetricInfoAPIResponse,
     getMetricsPerFilterAPIResponse,
   ];
@@ -35,7 +43,7 @@ describe("PerformanceAnalysis", () => {
   afterAll(() => server.close());
 
   it("should display two toggle buttons for the train and test", () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
     // verify if the toggles displayed with correct names
     expect(
       screen.getByRole("button", { name: "Evaluation Set", pressed: true })
@@ -46,10 +54,10 @@ describe("PerformanceAnalysis", () => {
   });
 
   it("should display the expected columns", async () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
     await waitFor(() => {
       const expectedColumnHeaders = [
-        "Label",
+        "filterValue",
         "Total",
         "Correct & Predicted",
         "Correct & Rejected",
@@ -71,13 +79,13 @@ describe("PerformanceAnalysis", () => {
   });
 
   it("should display the default sort as desc for colum header 'Total number of utterances'", () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
     const header = screen.getByRole("columnheader", { name: /Total/ });
     expect(header).toHaveAttribute("aria-sort", "descending");
   });
 
   it("should display the list of options if the user click on the column header 'filterValue'", () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
     fireEvent.mouseDown(screen.getByRole("button", { name: "Label" }));
     const filterList = screen.getAllByRole("option");
 
@@ -102,20 +110,76 @@ describe("PerformanceAnalysis", () => {
       "aria-selected",
       "false"
     );
+  });
+});
 
-    expect(screen.getByRole("option", { name: "Prediction" })).toHaveAttribute(
-      "aria-selected",
-      "false"
-    );
-    fireEvent.click(screen.getByRole("option", { name: "Prediction" }));
-    expect(
-      screen.getByRole("columnheader", { name: "Prediction" })
-    ).toBeInTheDocument();
+describe("PerformanceAnalysisComparisonTableWithMultiPipeline", () => {
+  const handlers = [
+    getConfigMultipipelineAPIResponse,
+    getCustomMetricInfoAPIResponse,
+    getMetricsPerFilterAPIResponse,
+  ];
+  const server = setupServer(...handlers);
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it("should display pipeline select component if the config has more than one pipeline", async () => {
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
+    await screen.findByText("Compare Baseline (Pipeline_0) with:");
   });
 
-  it("should not display the Footer component if the number of rows is lesser than initial number", async () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
-    await waitFor(() => expect(screen.queryByText(/See more/)).toBeNull());
+  it("should display list of pipelines", async () => {
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
+
+    await waitFor(() => {
+      fireEvent.mouseDown(screen.getByRole("button", { name: "No pipeline" }));
+      const pipelineList = screen.getAllByRole("option");
+      const expectedPipelineList = [
+        "No pipeline",
+        "Pipeline_0",
+        "Pipeline_1",
+        "Pipeline_2",
+        "Pipeline_3",
+      ];
+      expect(pipelineList).toHaveLength(expectedPipelineList.length);
+      pipelineList.forEach((item, index) => {
+        expect(item.textContent).toBe(expectedPipelineList[index]);
+      });
+      expect(screen.getByRole("option", { name: "Pipeline_0" })).toHaveClass(
+        "Mui-disabled"
+      );
+    });
+  });
+
+  it("should display the expected columns", async () => {
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
+    fireEvent.mouseDown(
+      await screen.findByRole("button", { name: "No pipeline" })
+    );
+    fireEvent.click(await screen.findByRole("option", { name: "Pipeline_1" }));
+    await waitFor(() => {
+      const expectedColumnHeaders = [
+        "filterValue",
+        "Total",
+        ...[
+          "Correct & Predicted",
+          "Correct & Rejected",
+          "Incorrect & Rejected",
+          "Incorrect & Predicted",
+          "Accuracy",
+          "Precision",
+          "Recall",
+          "F1",
+          "ECE",
+        ].flatMap((header) => ["Pipeline_0" + header, "Pipeline_1", "Delta"]),
+      ];
+      const actualColumnHeaders = screen.getAllByRole("columnheader");
+      expectedColumnHeaders.forEach((name, index) => {
+        expect(actualColumnHeaders[index].textContent).toBe(name);
+      });
+    });
   });
 });
 
@@ -123,14 +187,16 @@ describe("MetricsPerFilterAPIWithFailureResponse", () => {
   const handlers = [
     getMetricsPerFilterAPIWithFailureResponse,
     getCustomMetricInfoAPIResponse,
+    getConfigAPIResponse,
   ];
   const server = setupServer(...handlers);
 
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
+
   it("should display the error message if the API fails", async () => {
-    renderPerformanceAnalysis({ train: true, eval: true });
+    renderPerformanceAnalysisTable({ train: true, eval: true }, "eval");
     await waitFor(() => {
       // expected error message
       expect(

webapp/src/components/PipelineSelect.test.tsx
@@ -1,0 +1,39 @@
+import { fireEvent, screen, waitFor, within } from "@testing-library/react";
+import PipelineSelect from "./PipelineSelect";
+import { renderWithTheme } from "mocks/utils";
+
+const handleOnChange = jest.fn();
+
+describe("PipelineSelect", () => {
+  const pipelines = [
+    { name: "Pipeline_0" },
+    { name: "Pipeline_1" },
+    { name: "Pipeline_2" },
+  ];
+  const allPipelines = [
+    "No pipeline",
+    "Pipeline_0",
+    "Pipeline_1",
+    "Pipeline_2",
+  ];
+  it("should list all the pipelines", () => {
+    renderWithTheme(
+      <PipelineSelect
+        selectedPipeline={undefined}
+        onChange={handleOnChange}
+        pipelines={pipelines}
+        disabledPipelines={[]}
+      />
+    );
+    fireEvent.mouseDown(screen.getByRole("button", { name: "No pipeline" }));
+    const pipelineListOptions = screen.getAllByRole("option");
+
+    expect(pipelineListOptions).toHaveLength(allPipelines.length);
+    pipelineListOptions.forEach((item, index) => {
+      expect(item.textContent).toBe(allPipelines[index]);
+    });
+
+    fireEvent.click(screen.getByRole("option", { name: "Pipeline_0" }));
+    expect(handleOnChange).toBeCalledWith(0);
+  });
+});

webapp/src/mocks/api/mockDatasetInfoAPI.ts
@@ -1,0 +1,75 @@
+import { rest } from "msw";
+import { DatasetInfoResponse } from "types/api";
+
+const baseUrl = "http://localhost/api/local";
+
+export const getDatasetInfoAPIResponse = rest.get(
+  `${baseUrl}/dataset_info`,
+  (req, res, ctx) => {
+    const datasetInfoResponse: DatasetInfoResponse = {
+      projectName: "Sentiment Analysis",
+      classNames: ["negative", "positive", "REJECTION_CLASS"],
+      dataActions: [
+        "relabel",
+        "augment_with_similar",
+        "define_new_class",
+        "merge_classes",
+        "remove",
+        "investigate",
+        "NO_ACTION",
+      ],
+      smartTags: [
+        "multiple_sentences",
+        "long_utterance",
+        "short_utterance",
+        "missing_subj",
+        "missing_obj",
+        "missing_verb",
+        "conflicting_neighbors_train",
+        "conflicting_neighbors_eval",
+        "no_close_train",
+        "no_close_eval",
+        "failed_punctuation",
+        "failed_fuzzy_matching",
+        "high_epistemic_uncertainty",
+        "correct_top_3",
+        "correct_low_conf",
+        "incorrect_for_all_pipelines",
+        "pipeline_disagreement",
+        "NO_SMART_TAGS",
+      ],
+      evalClassDistribution: [428, 444, 0],
+      trainClassDistribution: [458, 542, 0],
+      startupTasks: {
+        syntax_tags_eval: "finished",
+        syntax_tags_train: "finished",
+        prediction_eval: "finished",
+        prediction_train: "finished",
+        saliency_eval: "finished",
+        saliency_train: "finished",
+        outcome_count_eval: "finished",
+        outcome_count_train: "finished",
+        confidence_bins_eval: "finished",
+        confidence_bins_train: "finished",
+        metrics_by_filter_eval: "finished",
+        metrics_by_filter_train: "finished",
+        perturbation_testing_eval: "finished",
+        perturbation_testing_train: "finished",
+        perturbation_testing_summary_all: "finished",
+        prediction_bma_eval: "finished",
+        prediction_bma_train: "finished",
+        outcome_count_per_threshold_eval: "finished",
+        neighbors_tags_eval: "not_started",
+        neighbors_tags_train: "not_started",
+        class_overlap_train: "not_started",
+      },
+      modelContract: "hf_text_classification",
+      predictionAvailable: true,
+      perturbationTestingAvailable: true,
+      availableDatasetSplits: { train: true, eval: true },
+      similarityAvailable: true,
+      postprocessingEditable: [true],
+    };
+    return res(ctx.json(datasetInfoResponse));
+  }
+);

commit 940b9cd6bb3e0f871accc4f72ec1179db8781db7

Fix front-end tests
The problem was caused by #420 and #466 having incompatible changes (but yet no conflicts).

webapp/src/mocks/api/mockConfigAPI.ts
@@ -4,7 +4,7 @@ import { AzimuthConfig } from "types/api";
 const baseUrl = "http://localhost/api/local";
 
 export const getConfigAPIResponse = rest.get(
-  `${baseUrl}/admin/config`,
+  `${baseUrl}/config`,
   (req, res, ctx) => {
     const azimuthConfigResponse: AzimuthConfig = {
       name: "Sentiment Analysis",
@@ -121,7 +121,7 @@ export const getConfigAPIResponse = rest.get(
 );
 
 export const getConfigMultipipelineAPIResponse = rest.get(
-  `${baseUrl}/admin/config`,
+  `${baseUrl}/config`,
   (req, res, ctx) => {
     const azimuthConfigMultipipelineResponse: AzimuthConfig = {
       name: "Sentiment Analysis",