180d3bce99d064f695b1d294f9ab2addd966bc23
Gabrielle Gauthier Melancon <gabrielle.gm@servicenow.com>
2023-03-15 17:06:33 -0400

    Hide TF info messages in logs (#492)

--- azimuth/__init__.py
+++ azimuth/__init__.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from datasets import disable_progress_bar
@@ -10,3 +11,4 @@ PROJECT_ROOT = str(Path(__file__).parent)
 set_verbosity_error()
 disable_progress_bar()
 set_logger_config()
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # INFO messages from TensorFlow are not printed
