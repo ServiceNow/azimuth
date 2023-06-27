17628471becc93e081e7019b731b255a828ffc1e
Joseph Marinier <joseph.marinier@servicenow.com>
2023-06-26 20:28:12 -0400

    Clean up importing routers (#585)
    removing 13 places where we needed `as`.

--- azimuth/routers/config.py
+++ azimuth/routers/config.py
@@ -1,7 +1,6 @@
 # Copyright ServiceNow, Inc. 2021 – 2022
 # This source code is licensed under the Apache 2.0 license found in the LICENSE file
 # in the root directory of this source tree.
-import os
 from typing import Dict, List
 
 import structlog
