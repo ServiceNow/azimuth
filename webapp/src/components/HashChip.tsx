c666265ecdbbed26d04b204878bcc52e13ddd99b
joseph.marinier <joseph.marinier@servicenow.com>
2023-06-27 21:18:14 -0400

    Improve hash chip
    - Fix cursor
    - Add tooltip explaining the hash

--- webapp/src/components/HashChip.tsx
+++ webapp/src/components/HashChip.tsx
@@ -1,16 +1,19 @@
-import { Chip } from "@mui/material";
+import { Chip, Tooltip } from "@mui/material";
 import React from "react";
 
 const HashChip: React.FC<{ hash: string }> = ({ hash }) => (
-  <Chip
-    size="small"
-    label={hash}
-    sx={{
-      fontFamily: "Monospace",
-      backgroundColor: `#${hash}`,
-      color: (theme) => theme.palette.getContrastText(`#${hash}`),
-    }}
-  />
+  <Tooltip title="This hash of the config lets you see when the same config appears multiple times.">
+    <Chip
+      size="small"
+      label={hash}
+      sx={(theme) => ({
+        backgroundColor: `#${hash}`,
+        color: theme.palette.getContrastText(`#${hash}`),
+        cursor: "unset",
+        fontFamily: "Monospace",
+      })}
+    />
+  </Tooltip>
 );
 
 export default React.memo(HashChip);
